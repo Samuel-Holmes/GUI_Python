38d9e965d965a3d1df0199cb97c251840ac3bbf0
Samuel-Holmes <samuel_n_holmes@outlook.com>
2025-06-14 19:25:03 +0100

    creating and placing test label

--- GUI.py
+++ GUI.py
@@ -2,4 +2,21 @@
 
 from tkinter import * 
 
+# Creating the window
+
 window = Tk()
+
+# This is the title at the top of the window similar to a tab title within HTML
+
+window.title('My first GUI')
+
+# This label is created and then must be placed within the window itself
+
+label = Label(window, text = 'Hello World!')
+
+# For placing the label I have used place which takes both XY coordinates, however there is also pack and grid options 
+
+label.place(x= 50, y = 50)
+
+
+window.mainloop()
